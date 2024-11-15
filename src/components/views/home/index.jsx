9bd02b03646dbd6feea75e965beeb18c8f937340
itsWalkerRichard <walker@teamrichard.com>
2024-11-15 16:50:21 -0700

    changed cover to image

--- src/components/views/home/index.jsx
+++ src/components/views/home/index.jsx
@@ -8,37 +8,37 @@ export default function HomeView () {
     {
       title: 'The Great Gatsby',
       author: 'F. Scott Fitzgerald',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of the mysteriously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan.',
     },
     {
       title: 'To Kill a Mockingbird',
       author: 'Harper Lee',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of young Scout Finch and her father, lawyer Atticus Finch, as he defends',
     },
     {
       title: '1984',
       author: 'George Orwell',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'A dystopian novel set in a totalitarian society where "Big Brother" watches over citizens.',
     },
     {
       title: 'The Great Gatsby 2',
       author: 'F. Scott Fitzgerald',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of the mysteriously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan.',
     },
     {
       title: 'To Kill a Mockingbird 2',
       author: 'Harper Lee',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of young Scout Finch and her father, lawyer Atticus Finch, as he defends',
     },
     {
       title: '1984 2',
       author: 'George Orwell',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'A dystopian novel set in a totalitarian society where "Big Brother" watches over citizens.',
     },
   ];
@@ -46,19 +46,19 @@ export default function HomeView () {
     {
       title: 'Pride and Prejudice',
       author: 'Jane Austen',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of Elizabeth Bennet and her love-hate relationship with Mr. Darcy.',
     },
     {
       title: 'The Catcher in the Rye',
       author: 'J.D. Salinger',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of Holden Caulfield, a teenager navigating the challenges of growing up.',
     },
     {
       title: 'Moby-D',
       author: 'Herman Melville',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of Captain Ahab and his obsessive...',
     },
   ];
@@ -66,19 +66,19 @@ export default function HomeView () {
     {
       title: 'the Hobbit',
       author: 'J.R.R. Tolkien',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of Bilbo Baggins and his unexpected adventure with a group of dwarves.',
     },
     {
       title: 'The Picture of Dorian Gray',
       author: 'Oscar Wilde',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of... ',
     },
     {
       title: 'Frankenstein',
       author: 'Mary Shelley',
-      cover: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
+      image: 'https://m.media-amazon.com/images/I/81ewpuVnBKL._AC_UF1000,1000_QL80_.jpg',
       description: 'The story of a Monster...',
     },
   ];
